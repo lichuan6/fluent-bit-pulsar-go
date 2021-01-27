a58629a545ede7f6d6f95b564ac2359302918475
x <x>
2021-01-27 16:26:29 +0800

    feat: log config when plugin init

--- main.go
+++ main.go
@@ -28,12 +28,22 @@ func init() {
 
 func initPulsarClient(url string, token string) error {
 	var err error
-	client, err = pulsar.NewClient(pulsar.ClientOptions{
-		URL:               url,
-		OperationTimeout:  30 * time.Second,
-		ConnectionTimeout: 30 * time.Second,
-		Authentication:    pulsar.NewAuthenticationToken(token),
-	})
+	option := pulsar.ClientOptions{}
+	if token == "" {
+		option = pulsar.ClientOptions{
+			URL:               url,
+			OperationTimeout:  30 * time.Second,
+			ConnectionTimeout: 30 * time.Second,
+		}
+	} else {
+		option = pulsar.ClientOptions{
+			URL:               url,
+			OperationTimeout:  30 * time.Second,
+			ConnectionTimeout: 30 * time.Second,
+			Authentication:    pulsar.NewAuthenticationToken(token),
+		}
+	}
+	client, err = pulsar.NewClient(option)
 	if err != nil {
 		log.Fatalf("Could not instantiate Pulsar client: %v", err)
 	}
@@ -69,6 +79,9 @@ func FLBPluginInit(plugin unsafe.Pointer) int {
 	token := output.FLBPluginConfigKey(plugin, "token")
 	tenant := output.FLBPluginConfigKey(plugin, "tenant")
 	namespace := output.FLBPluginConfigKey(plugin, "namespace")
+
+	log.Printf("[FLBPluginInit]url: %s\ntoken: %s\ntenant: %s\nnamespace: %s\n", url, token, tenant, namespace)
+	fmt.Printf("[FLBPluginInit]url: %s\ntoken: %s\ntenant: %s\nnamespace: %s\n", url, token, tenant, namespace)
 
 	// Set the context to point to any Go variable
 	configContext["url"] = url
@@ -158,6 +171,7 @@ func FLBPluginFlushCtx(ctx, data unsafe.Pointer, length C.int, tag *C.char) int 
 				sb.WriteString(fmt.Sprintf("\"%s\": %v, ", k, v))
 			}
 			log.Printf("tag: %s, k8s namespace: %s, topic: %s, record: %s\n", fbTag, k8sNamespace, topic, sb.String())
+			fmt.Printf("tag: %s, k8s namespace: %s, topic: %s, record: %s\n", fbTag, k8sNamespace, topic, sb.String())
 		}
 		// the type of record is map[interface{}]interface{}
 		// in order to serialize and send to pulsar
